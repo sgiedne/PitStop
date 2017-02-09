1c8fd93ef1c7e6a227a1498265770c2434a04b9b
Santhosh Subramanian <Santhosh Subramanian>
2017-02-08 20:23:02 -0600

    Global Modules || Santhosh || Feb 8 2017

---
+++ docs/modules/pitstop-footer-module/js/pitstop-footer.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	console.log('Inside document ready');
+   /*  $("expandGlobalSearch").click(function(){
+        $("up-arrow-image-section").show();
+		$("down-arrow-image-section").hide();
+    }); */
+});
