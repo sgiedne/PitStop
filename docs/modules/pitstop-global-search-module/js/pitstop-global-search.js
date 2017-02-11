857ec3a50ef7d7f686f940f26c6af32c8f38f11f
Siddharth <sgiedne@gmail.com>
2017-02-11 15:30:46 -0600

    Updated Global Search module|| Sidd || Feb 11 2017

--- docs/modules/pitstop-global-search-module/js/pitstop-global-search.js
+++ docs/modules/pitstop-global-search-module/js/pitstop-global-search.js
@@ -1,23 +1,39 @@
-$(document).ready(function(){
-	console.log('Inside pitstop global search document ready');
-	var parentDiv = $('#accordion div'),
-	childDiv = $('#accordion h4').siblings('div');
-	
-	$('#accordion p').click(function(){
-		parentDiv.slideUp();
-		if($(this).next().is(':hidden')){
-			$(this).next().slideDown();
-		}else{
-			$(this).next().slideUp();
-		}
-	});
-	
-	$('#accordion h4').click(function(){
-		childDiv.slideUp();
-		if($(this).next().is(':hidden')){
-			$(this).next().slideDown();
-		}else{
-			$(this).next().slideUp();
-		}
-	});
+$(document).ready(function () {
+    
+    var headers = ["H1", "H2", "H3", "H4", "H5", "H6"];
+    
+    //  $(".accordion p, .accordion div").slideUp("fast");
+    //  $(".accordion p, .accordion div").hide();
+    
+    $(".accordion p, .accordion div").hide();
+    $(".accordion").click(function (e) {
+        var target = e.target
+            , name = target.nodeName.toUpperCase();
+    
+        console.log("HERE------");
+        console.log(name);
+        console.log(target);
+        console.log($(target).next());
+        
+        if ($.inArray(name, headers) > -1) {
+            var subItem = $(target).next();
+            var depth = $(subItem).parents().length;
+            var allAtDepth = $(".accordion p, .accordion div").filter(function () {
+                if ($(this).parents().length >= depth && this !== subItem.get(0)) {
+                    return true;
+                }
+            });
+        
+            $(allAtDepth).slideUp("fast");
+            
+            
+            subItem.slideToggle("fast", function () {
+                $(".accordion :visible:last").css("border-radius", "0 0 10px 10px");
+            });
+            $(target).css({
+                "border-bottom-right-radius": "0"
+                , "border-bottom-left-radius": "0"
+            });
+        }
+    });
 });
