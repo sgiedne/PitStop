30c47156eb3a48667b0f7ede0b03fa68e6a7ebf0
Santhosh Subramanian <Santhosh Subramanian>
2017-02-13 18:07:52 -0600

    checklist merge || Santhosh || Feb 13 2017

---
+++ docs/modules/pitstop-landing-module/js/pitstop-landing.js
@@ -0,0 +1,60 @@
+var headers = ["H1", "H2", "H3", "H4", "H5", "H6"];
+$(document).ready(function(){
+	console.log('Inside document ready');
+	
+	$('.list-group-item').on('click', function() {
+		var $this = $(this);
+		var $alias = $this.data('alias');
+
+		$('.active').removeClass('active');
+		$this.toggleClass('active');
+	});
+	
+	$('#expandGlobalSearch').click(function(){
+		$('.up-arrow-image-section').css('display','inline');
+		$('.down-arrow-image-section').hide();
+		$('.global-search-section').show();
+	}); 
+
+	$('#collapseGlobalSearch').click(function(){
+		$('.up-arrow-image-section').css('display','none');
+		$('.down-arrow-image-section').show();
+		$('.global-search-section').hide();
+	}); 
+	
+	
+    //  $(".accordion p, .accordion div").slideUp("fast");
+    //  $(".accordion p, .accordion div").hide();
+    
+    $(".accordion p, .accordion div").hide();
+    $(".accordion").click(function (e) {
+        var target = e.target
+            , name = target.nodeName.toUpperCase();
+    
+        console.log("HERE------");
+        console.log(name);
+        console.log(target);
+        console.log($(target).next());
+        
+        if ($.inArray(name, headers) > -1) {
+            var subItem = $(target).next();
+            var depth = $(subItem).parents().length;
+            var allAtDepth = $(".accordion p, .accordion div").filter(function () {
+                if ($(this).parents().length >= depth && this !== subItem.get(0)) {
+                    return true;
+                }
+            });
+        
+            $(allAtDepth).slideUp("fast");
+            
+            
+            subItem.slideToggle("fast", function () {
+                $(".accordion :visible:last").css("border-radius", "0 0 10px 10px");
+            });
+            $(target).css({
+                "border-bottom-right-radius": "0"
+                , "border-bottom-left-radius": "0"
+            });
+        }
+    });
+});
